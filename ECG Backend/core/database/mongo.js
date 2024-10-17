daa96405ff3455868e85210a05d256263e4109bc
aten040791 <aten040791@gmail.com>
2024-10-17 09:57:10 +0700

    Fix mongodb server

--- ECG Backend/core/database/mongo.js
+++ ECG Backend/core/database/mongo.js
@@ -4,11 +4,11 @@ const configs = require('../../configs/index')
 const {user, password, host,port,database } = configs.database[configs.database.default].mongodb
 
 let mongoInstance;
-
+const connectionString = `mongodb://${user}:${password}@${host}:${port}/${database}?authSource=admin`
 try {
-    const connectionString = `mongodb://${user}:${password}@${host}:${port}/${database}?authSource=admin`
     mongoInstance = mongoose.createConnection(connectionString)
 } catch (e) {
+    console.log(connectionString);
     console.error("Mongodb connection error", e.message);
 }
 
