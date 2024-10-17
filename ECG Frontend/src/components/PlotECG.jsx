b57af63c1287458a48c3875c9086a7355a5cd557
aten040791 <aten040791@gmail.com>
2024-10-17 11:11:19 +0700

    Fix code bug

--- ECG Frontend/src/components/PlotECG.jsx
+++ ECG Frontend/src/components/PlotECG.jsx
@@ -59,8 +59,8 @@ const PlotECGDiagram = ({params}) => {
         <Plot
             data={[
                 {
-                    x: [...Array(measurementData?.ecg.length).keys().map(item => item + 1)],
-                    y: measurementData?.ecg,
+                    x: [...Array(measurementData.ecg.length).keys().map(item => item + 1)],
+                    y: measurementData.ecg,
                     mode: 'lines',
                     line: { color: 'blue' },
                 },
