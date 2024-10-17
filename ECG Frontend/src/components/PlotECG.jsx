2f805ddf0de3b3241bdf0e79f4b2cd93e0115aa4
aten040791 <aten040791@gmail.com>
2024-10-17 11:03:43 +0700

    Fix code bug

--- ECG Frontend/src/components/PlotECG.jsx
+++ ECG Frontend/src/components/PlotECG.jsx
@@ -1,46 +1,99 @@
-import Plot from 'react-plotly.js';
+import { useEffect, useState } from 'react';
 
-const PlotECGDiagram = ({y}) => {
+import Plot from 'react-plotly.js';
+import insertUrlParams from 'inserturlparams';
+import { localAxiosInstance } from '../../_api/axiosInstance';
+import routes from '../routes';
+
+// eslint-disable-next-line react/prop-types
+const PlotECGDiagram = ({params}) => {
+
+    const [measurementData, setMeasurementData] = useState({})
+
+    const [loading, setLoading] = useState(false);
+
+    const [, setMeasurementEcgLength] = useState(0)
+
+    const [needUpdateInterval, setNeedUpdateInterval] = useState(true);
+
+    useEffect(() => {
+        const fetchTest = async () => {
+            setLoading(true);
+            const response = await localAxiosInstance.get(
+                // eslint-disable-next-line react/prop-types
+                insertUrlParams(routes.api.measurements.show, {patientId: params.patientId, measurement: params.measurement})
+            );
+
+            if (response.success) {
+                setLoading(false);
+                setMeasurementData(response.data);
+                //check need to update or not
+                setMeasurementEcgLength(prevState => {
+                    if (response.data.heartRates.length === prevState) {
+                        setNeedUpdateInterval(false)
+                        return prevState;
+                    } else {
+                        return response.data.heartRates.length 
+                    }
+                })
+
+            }
+        };
+
+        if (needUpdateInterval) { // Only set interval if updates are needed
+            const updateInterval = setInterval(() => {
+                fetchTest();
+            }, 1500);
+
+            return () => clearInterval(updateInterval);
+        }
+
+    }, [needUpdateInterval]);
+    
     return (
+        <>
+        {loading ? <div>Loading...</div> :
         <Plot
-        data={[
-            {
-                x: [...Array(y.length).keys().map(item => item + 1)],
-                y: y,
-                mode: 'lines',
-                line: { color: 'blue' },
-            },
-        ]}
-        layout={{
-            title: 'ECG Signal',
-            xaxis: {
-                rangeslider: { visible: true },  // Enable the range slider
-                rangeselector: {               // Add range selector buttons
-                    buttons: [
-                        {
-                            count: 1,
-                            label: '1s',
-                            step: 'second',
-                            stepmode: 'backward',
-                        },
-                        {
-                            count: 10,
-                            label: '10s',
-                            step: 'second',
-                            stepmode: 'backward',
-                        },
-                        { step: 'all', label: 'All' },
-                    ],
+            data={[
+                {
+                    x: [...Array(measurementData?.ecg.length).keys().map(item => item + 1)],
+                    y: measurementData?.ecg,
+                    mode: 'lines',
+                    line: { color: 'blue' },
                 },
-            },
-            yaxis: {
-                fixedrange: false,  // Allow zooming on the Y-axis
-            },
-            autosize: true,
-        }}
-        config={{ responsive: true }}
-        style={{ width: "100%", height: "850px" }}
-        />
+            ]}
+            layout={{
+                title: 'ECG Signal',
+                xaxis: {
+                    rangeslider: { visible: true },  // Enable the range slider
+                    rangeselector: {               // Add range selector buttons
+                        buttons: [
+                            {
+                                count: 1,
+                                label: '1s',
+                                step: 'second',
+                                stepmode: 'backward',
+                            },
+                            {
+                                count: 10,
+                                label: '10s',
+                                step: 'second',
+                                stepmode: 'backward',
+                            },
+                            { step: 'all', label: 'All' },
+                        ],
+                    },
+                },
+                yaxis: {
+                    fixedrange: false,  // Allow zooming on the Y-axis
+                },
+                autosize: true,
+            }}
+            config={{ responsive: true }}
+            style={{ width: "100%", height: "850px" }}
+            />
+            }
+        </>
     )
 }
 
--- ECG Frontend/src/modules/Measurements/MeasurementDetail.jsx
+++ ECG Frontend/src/modules/Measurements/MeasurementDetail.jsx
@@ -1,9 +1,6 @@
-import { useEffect, useState } from 'react';
 import {useLocation, useNavigate, useParams} from 'react-router-dom';
 
 import PlotECGDiagram from '../../components/PlotECG';
-import insertUrlParams from 'inserturlparams';
-import { localAxiosInstance } from '../../_api/axiosInstance';
 import routes from '../../routes';
 
 const MeasurementDetail = () => {
@@ -15,68 +12,24 @@ const MeasurementDetail = () => {
 
     const params = useParams()
 
-    const [measurementData, setMeasurementData] = useState({})
-
-    const [loading, setLoading] = useState(false);
-
-    const [, setMeasurementEcgLength] = useState(0)
-
-    const [needUpdateInterval, setNeedUpdateInterval] = useState(true);
-
-    useEffect(() => {
-        const fetchTest = async () => {
-            setLoading(true);
-            const response = await localAxiosInstance.get(
-                insertUrlParams(routes.api.measurements.show, {patientId: params.patientId, measurement: params.measurement})
-            );
-
-           if (response.success) {
-               setLoading(false);
-               setMeasurementData(response.data);
-               //check need to update or not
-               setMeasurementEcgLength(prevState => {
-                   if (response.data.heartRates.length === prevState) {
-                       setNeedUpdateInterval(false)
-                       return prevState;
-                   } else {
-                       return response.data.heartRates.length 
-                   }
-               })
-
-           }
-        };
-
-        if (needUpdateInterval) { // Only set interval if updates are needed
-            const updateInterval = setInterval(() => {
-                fetchTest();
-            }, 1500);
-
-            return () => clearInterval(updateInterval);
-        }
-
-    }, [needUpdateInterval]);
-
 
     return (
         <div className="container mt-4">
             <h2 className="mb-4">Medical Test Details</h2>
-            <div className="card">
-                {
-                    loading ? <div>Loading...</div> :
-                    <>
-                        <div className="card-header">
-                            <h4>Measurement time: {params.measurement}</h4>
-                        </div>
-                        <div className="card-body">
-                            <PlotECGDiagram y={measurementData?.ecg || []}/>
-                        </div>
-                        <div className="card-footer">
-                            <button type="button" className="btn btn-secondary" onClick={() => navigate(`${routes.browser.home}?project_id=${projectId}&subject_id=${subjectId}`)}>
-                                Back
-                            </button>
-                        </div>
-                    </>
-                }
+            <div className="card">         
+                <>
+                    <div className="card-header">
+                        <h4>Measurement time: {params.measurement}</h4>
+                    </div>
+                    <div className="card-body">
+                        <PlotECGDiagram params={params}/>
+                    </div>
+                    <div className="card-footer">
+                        <button type="button" className="btn btn-secondary" onClick={() => navigate(`${routes.browser.home}?project_id=${projectId}&subject_id=${subjectId}`)}>
+                            Back
+                        </button>
+                    </div>
+                </> 
             </div>
         </div>
     );
